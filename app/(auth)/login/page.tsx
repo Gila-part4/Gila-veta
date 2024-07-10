d34eafde804d8be94cad604accd399bbab44571c
Batrnan <tjdwo2323@naver.com>
2024-07-10 13:34:28 +0900

    fix: 파일 이름 수정

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Logo from '@/app/(auth)/_components/Logo';
-import RegisterLink from '@/app/(auth)/login/_components/RegisterLink';
+import Logo from '@/app/(auth)/_components/logo';
+import RegisterLink from '@/app/(auth)/login/_components/register-link';
 
 function Page() {
   return (
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Logo from '@/app/(auth)/_components/Logo';
-import LoginLink from '@/app/(auth)/register/_components/LoginLink';
+import Logo from '@/app/(auth)/_components/logo';
+import LoginLink from '@/app/(auth)/register/_components/login-link';
 
 function Page() {
   return (
