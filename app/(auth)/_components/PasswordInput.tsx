9393b95bd9922860a0b98c1ebf916a1c1d101db5
juan0444 <dhwjs5267@gmail.com>
2024-07-10 17:47:16 +0900

    fix: 로직 수정

--- app/(auth)/_components/PasswordInput.tsx
+++ app/(auth)/_components/PasswordInput.tsx
@@ -1,17 +1,34 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Eye, EyeOff } from 'lucide-react';
+import { forwardRef, useState } from 'react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Input, InputProps } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
 
-const PasswordInput = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+const PasswordInput = forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const disabled = props.value === '' || props.value === undefined || props.disabled;
 
   return (
-    <button type="button" onClick={() => setShowPassword((prev) => !prev)}>
-      {showPassword ? <EyeOff color="#A4A1AA" size={24} /> : <Eye color="#A4A1AA" size={24} />}
-    </button>
+    <div className="relative">
+      <Input type={showPassword ? 'text' : 'password'} ref={ref} {...props} />
+      <Button
+        type="button"
+        variant="ghost"
+        size="sm"
+        onClick={() => setShowPassword((prev) => !prev)}
+        disabled={disabled}
+      >
+        {showPassword && !disabled ? (
+          <EyeIcon aria-hidden="true" color="#A4A1AA" size={24} />
+        ) : (
+          <EyeOffIcon aria-hidden="true" color="#A4A1AA" size={24} />
+        )}
+      </Button>
+    </div>
   );
-}
+});
 
 PasswordInput.displayName = 'PasswordInput';
 
