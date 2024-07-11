80f6e63a15a90c601f9f3a9512097d408673d590
sj0724 <sj07245@naver.com>
2024-07-12 00:00:23 +0900

    feat: 드롭다운 sort 값 searchparams로 설정

--- app/(client)/(public)/(main)/_components/sort-dropdown.tsx
+++ app/(client)/(public)/(main)/_components/sort-dropdown.tsx
@@ -10,10 +10,24 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { createUrl } from '@/lib/utils';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 export default function SortDropDown() {
   const [position, setPosition] = useState('new');
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleDropdown = (sortvalue: string) => {
+    const newSortParams = new URLSearchParams(searchParams.toString());
+    if (searchParams.get('sort')) {
+      newSortParams.delete('sort');
+    } else {
+      newSortParams.set('sort', sortvalue);
+    }
+    router.push(createUrl('/', newSortParams));
+  };
 
   return (
     <DropdownMenu>
@@ -24,9 +38,15 @@ export default function SortDropDown() {
         <DropdownMenuLabel>정렬 방법</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-          <DropdownMenuRadioItem value="new">최신순</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="priceAsc">가격 높은 순</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="priceDesc">가격 낮은 순</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="new" onClick={() => handleDropdown('new')}>
+            최신순
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="priceAsc" onClick={() => handleDropdown('priceAsc')}>
+            가격 높은 순
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="priceDesc" onClick={() => handleDropdown('priceDesc')}>
+            가격 낮은 순
+          </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
--- app/(client)/(public)/(main)/page.tsx
+++ app/(client)/(public)/(main)/page.tsx
@@ -1,3 +1,10 @@
+import SortDropDown from './_components/sort-dropdown';
+
 export default function Page() {
-  return <div>This is Home</div>;
+  return (
+    <div>
+      This is Home
+      <SortDropDown />
+    </div>
+  );
 }
