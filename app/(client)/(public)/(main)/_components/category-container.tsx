ce0ff3f4d309df5846666eb5dffa84ac1fd30f63
sj0724 <sj07245@naver.com>
2024-07-13 10:41:59 +0900

    feat: 전체 카테고리 버튼 구현

--- app/(client)/(public)/(main)/_components/category-container.tsx
+++ app/(client)/(public)/(main)/_components/category-container.tsx
@@ -1,9 +1,11 @@
 import { CATEGORIES } from '@/constants';
 import CategoryButton from './category-button';
+import TotalCategoryButton from './total-category-button';
 
 export default function CategoryContainer() {
   return (
     <div className="flex">
+      <TotalCategoryButton />
       {CATEGORIES.map((item) => (
         <CategoryButton item={item} key={item} />
       ))}
