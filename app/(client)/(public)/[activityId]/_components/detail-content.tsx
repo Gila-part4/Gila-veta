3c39b580950c0190f15d917c18d22159291674b0
juan0444 <dhwjs5267@gmail.com>
2024-07-18 08:12:56 +0900

    fix: 타입 extends 추가

--- app/(client)/(public)/[activityId]/_components/detail-content.tsx
+++ app/(client)/(public)/[activityId]/_components/detail-content.tsx
@@ -1,48 +1,36 @@
-import { SubImage } from '@/type/activities';
+import Image from 'next/image';
+import { ActivityItem, SubImage } from '@/type/activities';
 import DetailTitle from '@/app/(client)/(public)/[activityId]/_components/detail-title';
 import DetailDescription from '@/app/(client)/(public)/[activityId]/_components/detail-description';
 import { DetailCarousel } from '@/components/ui/detail-carousel';
 
-interface Props {
-  data: {
-    id: number;
-    title: string;
-    category: string;
-    description: string;
-    address: string;
-    rating: number;
-    reviewCount: number;
-    bannerImageUrl: string;
-    subImages: SubImage[];
-  };
+interface Props extends ActivityItem {
+  subImages: SubImage[];
 }
 
-export default function DetailContent({ data }: Props) {
-  const {
-    id,
-    title,
-    category,
-    description,
-    address,
-    rating,
-    reviewCount,
-    bannerImageUrl,
-    subImages,
-  } = data;
-
-  const carouselImagesUrl = [{ id: id, imageUrl: bannerImageUrl }, ...subImages];
-
+export default function DetailContent({
+  title,
+  category,
+  description,
+  address,
+  rating,
+  reviewCount,
+  bannerImageUrl,
+  subImages,
+}: Props) {
   return (
-    <div className="m-40">
-      <DetailTitle
-        title={title}
-        category={category}
-        address={address}
-        rating={rating}
-        reviewCount={reviewCount}
-      />
-      <DetailCarousel carouselImagesUrl={carouselImagesUrl} />
-      <DetailDescription description={description} address={address} />
+    <div>
+      <div className="mt-10 mr-40 ml-40">
+        <DetailTitle
+          title={title}
+          category={category}
+          address={address}
+          rating={rating}
+          reviewCount={reviewCount}
+        />
+        <DetailCarousel subImages={subImages} />
+        <DetailDescription description={description} address={address} />
+      </div>
     </div>
   );
 }
